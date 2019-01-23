4dba744ce8e5d26dc5b529fe0cb68dfcd426ca1e
bear <bear@daixinlian.com>
2019-01-23 14:48:49 +0800

    eureka-server 注册中心， 实现 高可用

---
+++ src/test/java/com/zccpro/eureka/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,12 @@
+package com.zccpro.eureka.eurekaserver;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class EurekaserverApplicationTests {
+
+//	@Test
+	public void contextLoads() {
+	}
+
+}
+
